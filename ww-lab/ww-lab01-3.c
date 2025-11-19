ccbd734a8f6045200ac8c224d8abfffd7439239f
Paphangkorn <paphangkorn.xp@gmail.com>
2025-11-19 21:59:08 +0700

    Add files via upload

---
+++ ww-lab/ww-lab01-3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+    int num1 , num2 , operationCode ;
+    int result = 0 ;
+    printf( " %d " , result ) ;
+    if ( scanf( "%d %d %d" , &num1 , &num2 , &operationCode ) != 3 ) {
+        return 1 ;
+    }
+    switch (operationCode) {
+    case 1 :
+        result = num1 + num2 ;
+        printf( "%d" , result ) ;
+        break ;
+    case 2 :
+        result = num1 - num2 ;
+        printf( "%d" , result ) ;
+        break ;
+    case 3 :
+        result = num1 * num2 ;
+        printf( "%d" , result ) ;
+        break ;
+    case 4 :
+        result = num1 / num2 ;
+        printf( "%d" , result ) ;
+        break ;
+    default :
+        printf( "Invalid Operation" ) ;
+        break ;
+    }
+    
+    return 0 ;
+}
