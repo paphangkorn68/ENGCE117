5c694a0ec97999240274dfbf08e29c7b0eed18bf
Paphangkorn <paphangkorn.xp@gmail.com>
2025-11-19 20:50:50 +0700

    Update ww-lab01.c

--- ww-lab/ww-lab01.c
+++ ww-lab/ww-lab01.c
@@ -2,20 +2,20 @@
 
 int main() {
 
-    int N, i;                               
+    int N , i ;                               
 
     printf( "enter number\n" ) ;            //แสดงenter number
-    if (scanf("%d", &N) != 1 ) {             //เช็ค n เป็นตัวเลขหรือไม่
+    if ( scanf( "%d", &N ) != 1 ) {             //เช็ค n เป็นตัวเลขหรือไม่
         printf( "error" ) ;
         return 1 ;
     }else if ( N <= 0 ) {
-        printf( "error" ) ;
+        printf( "Error" ) ;
     } else{
         for( i = 0 ; i < N ; i++ ) {        //loobสำหลับแสดง hello loob ตามจำนวน n ที่ใส่เข้าไป
-            printf( "hello loob\n" ) ;
+            printf( "Hello Loop!\n" ) ;
         }                                   //จบfor
         
     }                                       //จบif
 
-    return 0;
+    return 0 ;
 }                                           //จบเมน
