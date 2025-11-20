bce96e74def4383c691d5eefe07db9bcec4c4846
Paphangkorn <paphangkorn.xp@gmail.com>
2025-11-20 16:21:45 +0700

    Add files via upload

---
+++ ww-lab/ww-lab01-7.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int customerType;
+    float consumption_kWh;
+    float totalBill = 0.0;
+    
+
+    if (scanf("%d %f", &customerType, &consumption_kWh) != 2) {
+        return 1 ;
+    } else if ( customerType == 1 ) {
+        if( consumption_kWh > 100 ) {
+            totalBill = consumption_kWh * 4 ;
+        }else{
+            totalBill = consumption_kWh * 3 ;
+        }
+    } else if ( customerType == 2 )
+    {
+        if( consumption_kWh > 500 ) {
+            totalBill = consumption_kWh * 6.5 ;
+        } else {
+            totalBill = consumption_kWh * 5 ;
+        }
+    } else {
+        printf( "Invalid Customer Type" ) ;
+    }
+    
+    if( totalBill > 0.0 || customerType == 3 ) { 
+        printf("%.2f\n", totalBill);
+    } 
+
+    return 0;
+}
