5f46d955bcc39ace220bff252d213a4322a5af98
Paphangkorn <paphangkorn.xp@gmail.com>
2025-11-21 14:43:59 +0700

    Add files via upload

---
+++ ww-lab/lab2/ww-lab02-02.c
@@ -0,0 +1,36 @@
+// Pre-code (โค้ดบังคับ)
+#include <stdio.h>
+
+int main() {
+    int N, i, quantity;
+    float unitPrice, itemCost, grandTotal = 0.0;
+    
+    // รับค่า N (จำนวนรายการ)
+    if (scanf("%d", &N) != 1) {
+        return 1;
+    }
+    
+    // TODO: Implement the for loop (N times)
+    // TODO: Inside the loop, receive unitPrice and quantity
+    // TODO: Implement the if-else discount logic and accumulate grandTotal
+    for ( i = 0 ; i < N ; i++ ) {
+        if (scanf("%f %d", &unitPrice , &quantity ) != 2) {
+            return 1;
+        } else {
+            if ( unitPrice >= 1000 ) {
+                itemCost = unitPrice ;
+                unitPrice = unitPrice * 0.1 ;
+                itemCost = itemCost - unitPrice ;
+                itemCost = itemCost * quantity ;
+            } else {
+                itemCost = unitPrice *quantity ;
+            }
+            
+        }
+        grandTotal += itemCost ;
+        
+        
+    }
+    printf("Grand Total: %.2f\n", grandTotal);
+    return 0;
+}
