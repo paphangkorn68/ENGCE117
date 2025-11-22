e7a27f64dc548bd3014e187a8d7122cdf2906e78
Paphangkorn <paphangkorn.xp@gmail.com>
2025-11-22 21:29:21 +0700

    Update ww-lab02-05.c

--- ww-lab/lab2/ww-lab02-05.c
+++ ww-lab/lab2/ww-lab02-05.c
@@ -1,13 +1,15 @@
+#include <stdio.h>
+
 int main() {
-    int score;
+    int score ;
     int countA = 0 , countB = 0 , countC = 0 , countD = 0 , countF = 0 ;
     
-    
-    if ( scanf( "%d" , &score ) != 1 ) {
-        return 1 ;
+    printf( "enter score\n" ) ;
+    while (scanf( "%d" , &score ) != 1 ) {
+        printf( "enter score only \n" ) ;
+        while(getchar() != '\n') ;
     }
 
-    
     while ( score != -1 ) {
         if ( score >= 80 ) {
             countA++ ;
