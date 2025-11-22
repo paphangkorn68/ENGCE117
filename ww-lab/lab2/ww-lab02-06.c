da5ea8225f43e033765d08ee9702a29df4e9194a
Paphangkorn <paphangkorn.xp@gmail.com>
2025-11-22 21:17:16 +0700

    Update ww-lab02-06.c

--- ww-lab/lab2/ww-lab02-06.c
+++ ww-lab/lab2/ww-lab02-06.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 
 int main() {
-    int N_MONTHS, month, successCount = 0;
-    float dailyDeposit, monthlyTotal;
     
+    int N_MONTHS , month , successCount = 0 , L = 1 ;
+    float in[N_MONTHS + 1 ] ;
+    float dailyDeposit , monthlyTotal ;
+     
     
-    if (scanf("%d", &N_MONTHS) != 1) {
-        return 1;
+    if ( scanf( "%d" , &N_MONTHS ) != 1 ) {
+        return 1 ;
     }
     
     
-    for (month = 1; month <= N_MONTHS; month++) {
-        monthlyTotal = 0.0;
+    for ( month = 1 ; month <= N_MONTHS ; month++ ) {
+        monthlyTotal = 0.0 ;
         
         
-        if (scanf("%f", &dailyDeposit) != 1) {
-             break;
+        if ( scanf( "%f" , &dailyDeposit ) != 1 ) {
+             break ;
         }
 
         
-        while (dailyDeposit != 0.00) {
+        while ( dailyDeposit != 0.00 ) {
             monthlyTotal += dailyDeposit ;
-            
-            if (scanf("%f", &dailyDeposit) != 1) {
-                break;
+            in[month] = monthlyTotal ;
+            if (scanf( "%f" , &dailyDeposit) != 1 ) {
+                break ;
             }
         }
         
         if ( monthlyTotal >= 500 ) {
                 successCount++ ;
             }
-        printf("Month %d Total: %.2f\n", month, monthlyTotal);
+        
     }
+    while ( L <= N_MONTHS ) {
+        printf( "Month %d Total: %.2f\n" , L , in[L] ) ;
+        L++ ;
+    }
+        
     
-    printf("Success Count: %d\n", successCount);
-    return 0;
+    printf( "Success Count: %d\n" , successCount ) ;
+    return 0 ;
 }
