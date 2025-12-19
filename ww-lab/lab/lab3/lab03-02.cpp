745c7fa7c97ccb48d457739894aa0bb7292d8340
Paphangkorn <paphangkorn.xp@gmail.com>
2025-12-19 16:21:35 +0700

    Add files via upload

---
+++ ww-lab/lab/lab3/lab03-02.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct studen {
+    int age ;
+    char name[20] ;
+    char sex ;
+    float gpa ; 
+
+} ;
+
+void upgrade( struct studen *boy ) ;
+
+int main() {
+    struct studen boy ;
+    boy.sex = 'M' ;
+    boy.gpa = 3 ;
+
+    upgrade( &boy ) ;
+    printf( "gpa = %.2f" , boy.gpa ) ;
+
+    return 0 ;
+}
+
+void upgrade( struct studen *boy ) {
+
+    (*boy).gpa += 10 ;
+
+}
